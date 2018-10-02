0d2a38f4d8f70930bb775fdb6d868877f35d9c9d
Austin <metalnut4@netscape.net>
2018-10-02 00:29:53 +0000

    Rework data distribution.
    Move data directory inside the routes1846 directory.

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(
     name='routes-1846',
     version='0.1',
-    packages=['routes1846']
+    packages=['routes1846'],
+    package_data={"routes1846": ["data/base-board.json", "data/tiles.json"]},
+    include_package_data=True
 )
