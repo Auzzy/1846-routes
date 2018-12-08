7bb5f333f3bdd2c35f7ae14572953cbc3b995461
Austin <metalnut4@netscape.net>
2018-12-08 04:07:58 +0000

    Expose logger.
    Allows outside users to latch onto the logger.

--- routes1846/__init__.py
+++ routes1846/__init__.py
@@ -6,4 +6,4 @@ _DATA_ROOT_DIR = os.path.abspath(os.path.normpath(os.path.join(os.path.dirname(_
 def get_data_file(filename):
     return os.path.join(_DATA_ROOT_DIR, filename)
 
-from routes1846.find_best_routes import find_best_routes
+from routes1846.find_best_routes import find_best_routes, LOG
